d4a8c6ce3f8af04a250cab36072096ead1f73152
maks2 <maks210306@yandex.by>
2025-03-16 23:13:41 +0300

    check style fix

--- src/main/java/com/myfinance/financetracker/service/impl/AccountServiceImpl.java
+++ src/main/java/com/myfinance/financetracker/service/impl/AccountServiceImpl.java
@@ -1,17 +1,16 @@
 package com.myfinance.financetracker.service.impl;
 
-import com.myfinance.financetracker.utils.InMemoryCache;
 import com.myfinance.financetracker.exception.ResourceNotFoundException;
 import com.myfinance.financetracker.model.Account;
 import com.myfinance.financetracker.model.User;
 import com.myfinance.financetracker.repository.AccountRepository;
 import com.myfinance.financetracker.repository.UserRepository;
 import com.myfinance.financetracker.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.myfinance.financetracker.utils.InMemoryCache;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class AccountServiceImpl implements AccountService {
--- src/main/java/com/myfinance/financetracker/utils/InMemoryCache.java
+++ src/main/java/com/myfinance/financetracker/utils/InMemoryCache.java
@@ -1,9 +1,8 @@
 package com.myfinance.financetracker.utils;
 
-import org.springframework.stereotype.Component;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Component;
 
 @Component
 public class InMemoryCache<K, V> {
